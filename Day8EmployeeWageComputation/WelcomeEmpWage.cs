bbcf8660d34401e1947e8b3c434e1d7296316daf
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-28 11:54:13 +0530

    ok

---
+++ Day8EmployeeWageComputation/WelcomeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class WelcomeEmpWage
+    {
+        public void display()
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation System");
+        }
+    }
+}
