72be211294c270004f3861a5cd9d9e8f75401deb
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-28 11:17:33 +0530

    ok

---
+++ Day9EmployeeWageComputation/EmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            int halfDayWage = 0, fullDayWage = 0, fullDayHour = 8, halfDayHour = 4, perHourWage = 20, month = 20, sal = 0, totalWage = 0,totalSal=0,n=0,attend=0;
+
+            halfDayWage = halfDayHour * perHourWage;
+            fullDayWage = fullDayHour * perHourWage;
+            Random rand = new Random();
+            Console.WriteLine("Enter value for n");
+            n = int.Parse(Console.ReadLine());
+            EmpAttendance[] obj = new EmpAttendance[n];
+            EmpSalary[] obj2 = new EmpSalary[n];
+            for (int j = 1; j <= n; j++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Enter Compony name");
+                String name = Console.ReadLine();
+                Console.WriteLine("\n");
+                Console.WriteLine("Compony Name : "+name);
+                for (int i = 1; i <= month; i++)
+                {
+                    attend = rand.Next(1, 4);
+                    obj[j].attendance(attend);
+                    sal = obj2[j].attendance(attend, halfDayWage, fullDayWage, halfDayWage, perHourWage, halfDayHour, totalWage);
+                    totalSal = totalSal + sal;
+                }
+                obj2[j].salary(totalSal);
+            }
+
+        }
+    }
+}
---
+++ Day9EmployeeWageComputation/WelcomeEmpWage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class WelcomeEmpWage
+    {
+    }
+}
