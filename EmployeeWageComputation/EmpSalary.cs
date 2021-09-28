d98b79524fe942021b9f0a33e2d04911cab4917e
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-28 10:26:11 +0530

    ok

---
+++ EmployeeWageComputation/EmpSalary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    public class EmpSalary:EmpAttendance
+    {
+         
+        public int attendance(int attend, int halfDayWage, int fullDayWage, int fullDayHour, int halfDayHour, int perHourWage,int totalWage)
+        {
+            
+            switch (attend)
+            {
+                case 0:
+                    Console.WriteLine("Day Eage : 00");
+                    break;
+                case 1:
+                    
+                    totalWage = totalWage + halfDayWage;
+                    Console.WriteLine("Half Day Wage : "+ halfDayWage);
+                    break;
+                case 2:
+                   
+                    totalWage = totalWage + fullDayWage;
+                    Console.WriteLine("Half Day Wage : " + fullDayWage);
+                    break;
+            }
+            
+            return totalWage;
+        }
+        public void salary(int totalWage)
+        {
+            Console.WriteLine("Monthly Salary : " + totalWage);
+
+        }
+
+    }
+}
---
+++ EmployeeWageComputation/EmpWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            int halfDayWage = 0, fullDayWage = 0, fullDayHour = 8, halfDayHour = 4, perHourWage = 20, month = 20, sal = 0, totalWage = 0,totalSal=0;
+
+            halfDayWage = halfDayHour * perHourWage;
+            fullDayWage = fullDayHour * perHourWage;
+            Random rand = new Random();
+            
+            EmpAttendance obj = new EmpAttendance();
+            EmpSalary obj2 = new EmpSalary();
+            for (int i = 1; i <= month; i++)
+            {
+                int attend = rand.Next(0, 3);
+                obj.attendance(attend);
+               sal = obj2.attendance(attend, halfDayWage, fullDayWage, halfDayWage, perHourWage, halfDayHour, totalWage);
+                totalSal = totalSal + sal;
+            }
+            obj2.salary (totalSal);
+
+        }
+    }
+}
