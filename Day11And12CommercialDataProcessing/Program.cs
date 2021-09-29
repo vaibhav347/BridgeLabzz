c426a9f971e01637582ecdf25c43c009800d3421
vaibhav347 <36009358+vaibhav347@users.noreply.github.com>
2021-09-29 14:21:10 +0530

    Add files via upload

---
+++ Day11And12CommercialDataProcessing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CommercialDataProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StockAccount obj1 = new StockAccount();
+            obj1.stockAccount();
+            obj1.transaction();
+        }
+    }
+}
---
+++ Day11And12CommercialDataProcessing/StockDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommercialDataProcessing
+{
+    interface StockDetails
+    {
+        public void stockAccount();
+        public void sell();
+        public void buy();
+        public void save();
+
+    }
+}
