a27cccf48b23d5cf0172db67dd451534b988233f
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-30 15:42:26 +0530

    ok

---
+++ Day11And12JSONInventoryDataManagementCase1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Day11And12JSONInventoryDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+
+            Rice ricedata = fetchJsonForRice.Read("C:/Users/3011313/Desktop/BridgeLabzz/Day11And12JSONInventoryDataManagementCase2/Rice.json");
+
+            for (int i = 0; i < ricedata.TypesOfPulses.Count; i++)
+            {
+                Console.WriteLine(ricedata.TypesOfPulses[i].name);
+                Console.WriteLine(ricedata.TypesOfPulses[i].Price);
+                Console.WriteLine(ricedata.TypesOfPulses[i].weight);
+                Console.WriteLine("------");
+            }
+
+           /* for (int i = 0; i < ricedata.TypesOfRice.Count; i++)
+            {
+                Console.WriteLine(ricedata.TypesOfRice[i].name);
+                Console.WriteLine(ricedata.TypesOfRice[i].Price);
+                Console.WriteLine(ricedata.TypesOfRice[i].weight);
+                Console.WriteLine("------");
+            }*/
+
+        }
+    }
+}
---
+++ Day11And12JSONInventoryDataManagementCase1/Rice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day11And12JSONInventoryDataManagement
+{
+    public class Rice
+    {
+        //public List<TypesOfRice> TypesOfRice;
+        public List<TypesOfPulses> TypesOfPulses;
+    }
+
+    /* public class TypesOfRice
+   {
+        public string name;
+        public string weight;
+        public int Price;
+    }*/
+
+    public class TypesOfPulses
+    {
+        public string name;
+        public string weight;
+        public int Price;
+    }
+}
