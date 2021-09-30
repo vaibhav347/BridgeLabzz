7fd77a332cc1142fcf1ac7d3343479877f484002
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-09-30 15:44:20 +0530

    ok

---
+++ Day11And12JSONInventoryDataManagementCase2/FetchJsonForRice.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day11And12JSONInventoryDataManagement
+{
+    class FetchJsonForRice
+    {
+        public Rice Read(string Path)
+        {
+
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+
+            }
+        }
+    }
+}
