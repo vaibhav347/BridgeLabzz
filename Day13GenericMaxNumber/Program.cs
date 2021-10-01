11806aea85f580ef6167e737fae24e2303e6ee56
Vaibhav Aher <vaibhavaher347@gmail.com>
2021-10-01 13:35:54 +0530

    ok

---
+++ Day13GenericMaxNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day13GenericMaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arri = { 10, 20, 30 };
+            double[] arrd = { 10.0, 20.0, 30.0 };
+            char[] arrc = { 'a', 'b' };
+            max obj = new max ();
+            
+            
+            obj.maxNum(arri);
+            obj.maxNum(arrd);
+            obj.maxNum(arrc);
+        }
+    }
+}
---
+++ Day13GenericMaxNumber/max.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day13GenericMaxNumber
+{
+    public class max
+    {
+
+        public void maxNum<T>(T[] arr)
+        {
+            T maximum = arr.Max();
+            Console.WriteLine("Maximum Number : " + maximum);
+            Console.WriteLine("------------------");
+
+        }
+    }
+}
